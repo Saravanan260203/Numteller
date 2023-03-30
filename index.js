f20f89bc8300122045320e92e1b76e157fc77871
Saravanan <124013302+Saravanan260203@users.noreply.github.com>
2023-03-30 11:22:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,134 @@
+function show0()
+{
+  document.getElementsByClassName("name")[0].style.display="block"
+var audio=new Audio("zero.mp3")
+audio.play()
+
+
+  document.getElementsByTagName("button")[0].classList.add("press")
+
+  setTimeout(function(){
+
+    document.getElementsByTagName("button")[0].classList.remove("press");
+  }, 175);
+}
+
+
+
+function show1()
+{
+  document.getElementsByClassName("name")[1].style.display="block"
+  var audio=new Audio("ONE.mp3")
+audio.play()
+document.getElementsByTagName("button")[1].classList.add("press")
+
+  setTimeout(function(){
+
+    document.getElementsByTagName("button")[1].classList.remove("press");
+  }, 175);
+
+}
+function show2()
+{
+  document.getElementsByClassName("name")[2].style.display="block"
+  var audio=new Audio("TWO.mp3")
+audio.play()
+document.getElementsByTagName("button")[2].classList.add("press")
+
+  setTimeout(function(){
+
+    document.getElementsByTagName("button")[2].classList.remove("press");
+  }, 175);
+
+}
+function show3()
+{
+  document.getElementsByClassName("name")[3].style.display="block"
+  var audio=new Audio("THREE.mp3")
+audio.play()
+document.getElementsByTagName("button")[3].classList.add("press")
+
+  setTimeout(function(){
+
+    document.getElementsByTagName("button")[3].classList.remove("press");
+  }, 175);
+
+}
+function show4()
+{
+  document.getElementsByClassName("name")[4].style.display="block"
+  var audio=new Audio("FOUR.mp3")
+audio.play()
+document.getElementsByTagName("button")[4].classList.add("press")
+
+  setTimeout(function(){
+
+    document.getElementsByTagName("button")[4].classList.remove("press");
+  }, 175);
+
+}
+function show5()
+{
+  document.getElementsByClassName("name")[5].style.display="block"
+  var audio=new Audio("FIVE.mp3")
+audio.play()
+
+document.getElementsByTagName("button")[5].classList.add("press")
+
+  setTimeout(function(){
+
+    document.getElementsByTagName("button")[5].classList.remove("press");
+  }, 175);
+
+}
+function show6()
+{
+  document.getElementsByClassName("name")[6].style.display="block"
+  var audio=new Audio("SIX.mp3")
+audio.play()
+document.getElementsByTagName("button")[6].classList.add("press")
+
+  setTimeout(function(){
+
+    document.getElementsByTagName("button")[6].classList.remove("press");
+  }, 175);
+
+}
+function show7()
+{
+  document.getElementsByClassName("name")[7].style.display="block"
+  var audio=new Audio("SEVEN.mp3")
+audio.play()
+document.getElementsByTagName("button")[7].classList.add("press")
+
+  setTimeout(function(){
+
+    document.getElementsByTagName("button")[7].classList.remove("press");
+  }, 175);
+
+}
+function show8()
+{
+  document.getElementsByClassName("name")[8].style.display="block"
+  var audio=new Audio("EIGHT.mp3")
+audio.play()
+document.getElementsByTagName("button")[8].classList.add("press")
+
+  setTimeout(function(){
+
+    document.getElementsByTagName("button")[8].classList.remove("press");
+  }, 175);
+}
+function show9()
+{
+  document.getElementsByClassName("name")[9].style.display="block"
+  var audio=new Audio("NINE.mp3")
+audio.play()
+document.getElementsByTagName("button")[9].classList.add("press")
+
+  setTimeout(function(){
+
+    document.getElementsByTagName("button")[9].classList.remove("press");
+  }, 175);
+}
+
